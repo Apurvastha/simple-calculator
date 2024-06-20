83d90d80c74eb2c74e4b270a9f3d332ce7fae822
Reaper <69500690+Apurvastha@users.noreply.github.com>
2024-06-20 23:42:09 +0545

    gradient finish

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ function calculate() {
         document.getElementById('screen').value = 'Error';
     }
 }
+function calculatePercent() {
+    let currentValue = parseFloat(document.getElementById('screen').value);
+    if (!isNaN(currentValue)) {
+        let result = currentValue / 100;
+        document.getElementById('screen').value = result;
+    }
+}
+
+function backspace() {
+    let currentValue = document.getElementById('screen').value;
+    document.getElementById('screen').value = currentValue.slice(0, -1);
+}
+
+// Connect buttons to numpad
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    const validKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*','%', '/', '.', 'Enter', 'Backspace', 'Escape'];
+    
+    if (validKeys.includes(key)) {
+        event.preventDefault();
+        if (key === 'Enter') {
+            calculate();
+        } else if (key === 'Backspace') {
+            backspace();
+        } else if (key === 'Escape') {
+            clearScreen();
+        } 
+        else if (key === '%') {
+            calculatePercent();
+        }
+        else {
+            appendValue(key);
+        }
+    }
+});
